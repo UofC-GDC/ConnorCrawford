6c62126be49f3de7e1c75814e697a34d8d87c609
CaelumSloan <caelum.sloan1@ucalgary.ca>
2019-02-12 14:43:25 -0700

    Actually fixed insight glitch this time

--- Assets/_Project/Scripts/Data/Insight.cs
+++ Assets/_Project/Scripts/Data/Insight.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class InsightOption
+{
+    public List<string> insightOption;
+}
+
 [CreateAssetMenu(fileName = "Insight", menuName ="New Insight")]
 public class Insight : ScriptableObject {
-    public List<List<string>> text = new List<List<string>>();
+
+    public List<InsightOption> text = new List<InsightOption>();
 
     public Text style;
 }
