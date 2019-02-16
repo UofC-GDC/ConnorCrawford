17bd5d4f0ccae0ced268bdc56d4fa8f56614f45b
Phantom-1st <caelum.sloan1@ucalgary.ca>
2019-02-16 14:04:03 -0700

    Added basic logo intro

---
+++ Assets/_Project/Scripts/LogoIntro/LogoIntro.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoIntro : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private float introTime = 5;
+    [SerializeField] private Animator panelAnimator;
+
+	void Start ()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        audioSource.Play();
+        StartCoroutine(Intro());
+    }
+
+    IEnumerator Intro()
+    {
+        yield return new WaitForSeconds(introTime/2);
+        panelAnimator.SetTrigger("FadeToBlack");
+        yield return new WaitForSeconds(introTime / 2);
+        SceneManager.LoadScene("Main");
+    }
+}
