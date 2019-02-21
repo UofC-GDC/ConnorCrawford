1ba53b1e46a7fcc7f3fa0ce1bf34db721327c0e2
caelum.sloan1 <caelum.sloan1@ucalgary.ca>
2019-02-20 17:16:01 -0700

    Updated LightSwitch

---
+++ Assets/_Project/Scripts/Things/LightSwitch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class LightSwitch : Thing
+{
+    [SerializeField] private Light roomLight;
+    [SerializeField] private Animator animator;
+
+    private void Start()
+    {
+        animator.SetTrigger("LightOn");
+        roomLight.enabled = true;
+    }
+
+    private bool lightOn;
+    private string triggerString = "";
+
+    public override State Action()
+    {
+        if (lightOn)
+        {
+            triggerString = "LightOn";
+            lightOn = roomLight.enabled = true;
+        }
+        else
+        {
+            triggerString = "LightOff";
+            lightOn = roomLight.enabled = false;
+        }
+
+        animator.SetTrigger(triggerString);
+
+        return null;
+    }
+}
