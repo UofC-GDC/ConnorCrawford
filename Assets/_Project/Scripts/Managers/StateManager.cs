00fc588623c961bad58f63c753110450d45d2ca3
Daniel Doran <ddoran3@gmail.com>
2019-02-27 18:08:08 -0700

    add new env fields

--- Assets/_Project/Scripts/Managers/StateManager.cs
+++ Assets/_Project/Scripts/Managers/StateManager.cs
@@ -4,40 +4,44 @@ using UnityEngine;
 
 /*
  * THE GAME
- * 
+ *
  * This drives the game, and is the main state machine as well as the data manager
  **/
-public class StateManager : Singleton<StateManager> {
+public class StateManager : Singleton<StateManager>
+{
 
-    State currentState;
-    State previousSate;
+	State currentState;
+	State previousSate;
 
-    public Env env {
-        get;
-        private set;
-    }
+	public Env env
+	{
+		get;
+		private set;
+	}
 
-    private void Awake()
-    {
-        previousSate = null;
-        currentState = new StartState(); // Begin
-    }
+	private void Awake()
+	{
+		previousSate = null;
+		currentState = new StartState(); // Begin
+	}
 
-    private void Update()
-    {
-        var prev = previousSate;
-        previousSate = currentState;
-        Env? next = null;
-        currentState = currentState.DoAction(prev, env, ref next); // State Transition
-        env = next ?? env;
-    }
+	private void Update()
+	{
+		var prev = previousSate;
+		previousSate = currentState;
+		Env? next = null;
+		currentState = currentState.DoAction(prev, env, ref next); // State Transition
+		env = next ?? env;
+	}
 
-    // The Game State
-    public struct Env
-    {
-        public Player player;
-        bool rightClicked;
-        bool leftClicked;
-    }
+	// The Game State
+	public struct Env
+	{
+		public Player player;
+		public bool rightClicked;
+		public bool leftClicked;
+		public Thing target;
+		public RaycastHit2D hit;
+	}
 
 }
