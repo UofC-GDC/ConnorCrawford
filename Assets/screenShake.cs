88807f6d7fa259e1c9ae28722b5f06bbb28cd14a
caelum.sloan1 <caelum.sloan1@ucalgary.ca>
2019-03-09 14:20:09 -0700

    ScreenShake

---
+++ Assets/screenShake.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenShake : MonoBehaviour
+{
+
+    [SerializeField] private float shakeMagnitude;
+    [SerializeField] private AnimationCurve damping;
+
+    private Vector3 initialPosition;
+    private float shakeDuration = 0;
+
+    [ContextMenu("TriggerShake")]
+    private void setTriggerShake() { TriggerShake(2f); }
+
+    public void TriggerShake(float shakeDuration)
+    {
+        this.shakeDuration = shakeDuration;
+    }
+
+    void OnEnable()
+    {
+        initialPosition = transform.localPosition;
+    }
+
+    void Update ()
+    {
+		if (shakeDuration > 0)
+        {
+           transform.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude * damping.Evaluate(shakeDuration);
+   
+           shakeDuration -= Time.deltaTime;
+        }
+        else
+        {
+           shakeDuration = 0f;
+           transform.localPosition = initialPosition;
+        }
+	}
+}
