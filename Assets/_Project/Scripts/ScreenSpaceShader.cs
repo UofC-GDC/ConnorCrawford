200292cdcd4cec9da9e7d146a65452842ce6e712
Riley Baker <vishnukey@gmail.com>
2019-03-10 14:58:51 -0600

    Screen Space shaders applied to camera and colour multiplier shader added

---
+++ Assets/_Project/Scripts/ScreenSpaceShader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSpaceShader : MonoBehaviour {
+
+    public List<Material> effects;
+
+    //void OnEnable()
+    //{
+    //    // dynamically create a material that will use our shader
+    //    _material = new Material(Shader.Find("TKoU/ScreenSpaceSnow"));
+
+    //    // tell the camera to render depth and normals
+    //    GetComponent<Camera>().depthTextureMode |= DepthTextureMode.DepthNormals;
+    //}
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        // set shader properties
+
+        // execute the shader on input texture (src) and write to output (dest)
+        //Graphics.Blit(src, dest, effect);
+        foreach (var effect in effects) Graphics.Blit(src, dest, effect);
+    }
+}
