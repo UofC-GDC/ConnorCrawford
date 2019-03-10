467b84ba4ee584e0609037c565a9518c33585e48
caelum.sloan1 <caelum.sloan1@ucalgary.ca>
2019-03-10 16:28:25 -0600

    Time machine

---
+++ Assets/BlinkingLight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkingLight : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer image;
+
+    private int frameCount = 0;
+
+	void Update ()
+    {
+        frameCount++;   
+
+        if (frameCount % 3 != 0) return;
+
+        var rand = Random.value;
+
+        switch (image.enabled)
+        {
+            case true:
+                if (rand < 0.75) image.enabled = false;
+                break;
+            case false:
+                if (rand > 0.75) image.enabled = true;
+                break;
+        }
+	}
+}
