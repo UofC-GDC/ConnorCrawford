922bd11320a56b86e8311ee22494d3c207f81335
caelum.sloan1 <caelum.sloan1@ucalgary.ca>
2019-03-09 17:44:23 -0700

    So many scene changes

--- Assets/_Project/Scripts/Things/Door.cs
+++ Assets/_Project/Scripts/Things/Door.cs
@@ -7,6 +7,8 @@ public class Door : Thing
     [SerializeField]    private AudioSource doorSource;
     [SerializeField]    private AudioClip doorOpenClip;
     [SerializeField]    private AudioClip doorCloseClip;
+    [SerializeField]    private BoxCollider2D openCollider;
+    [SerializeField]    private BoxCollider2D closedCollider;
 
     [SerializeField]    private bool locked = false;
     [HideInInspector]   public bool open = true;
@@ -27,6 +29,9 @@ public class Door : Thing
                 open = true;
                 collider3d.SetActive(true);
                 ChangeAndPlayClip(doorOpenClip);
+                transform.localRotation = Quaternion.Euler(-57, -90, 90);
+                openCollider.enabled = true;
+                closedCollider.enabled = false;
                 return null;
             }
         }
@@ -50,6 +55,9 @@ public class Door : Thing
         open = false;
         collider3d.SetActive(false);
         ChangeAndPlayClip(doorCloseClip);
+        openCollider.enabled = false;
+        closedCollider.enabled = true;
+        transform.localRotation = Quaternion.Euler(-57, 90, -90);
     }
 
     private void ChangeAndPlayClip(AudioClip clip)
