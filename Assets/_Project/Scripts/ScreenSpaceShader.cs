9dce03be5327e30ffca1fa37b75c4893eb4a6d61
Riley Baker <vishnukey@gmail.com>
2019-03-10 17:29:57 -0600

    Cleaned up ScreenSpaceShader.cs

--- Assets/_Project/Scripts/ScreenSpaceShader.cs
+++ Assets/_Project/Scripts/ScreenSpaceShader.cs
@@ -6,22 +6,8 @@ public class ScreenSpaceShader : MonoBehaviour {
 
     public Material effect;
 
-    //void OnEnable()
-    //{
-    //    // dynamically create a material that will use our shader
-    //    _material = new Material(Shader.Find("TKoU/ScreenSpaceSnow"));
-
-    //    // tell the camera to render depth and normals
-    //    GetComponent<Camera>().depthTextureMode |= DepthTextureMode.DepthNormals;
-    //}
-
     void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        // set shader properties
-
-        // execute the shader on input texture (src) and write to output (dest)
-        //Graphics.Blit(src, dest, effect);
-            Graphics.Blit(src, dest, effect);
- 
+        Graphics.Blit(src, dest, effect);
     }
 }
