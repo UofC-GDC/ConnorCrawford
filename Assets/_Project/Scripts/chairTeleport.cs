ef8c31b70ac5b53111d0608b1d7df4b706a51461
Phantom-1st <caelum.sloan1@ucalgary.ca>
2019-03-11 22:11:54 -0600

    Funniest thing I've ever added don't look into this commit one day youll discover what I added and laugh

---
+++ Assets/_Project/Scripts/chairTeleport.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chairTeleport : MonoBehaviour 
+{
+    [SerializeField] private Transform spawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Chair")) return;
+
+        var body = other.GetComponent<Rigidbody>();
+        body.velocity = Vector3.zero;
+        body.angularVelocity = Vector3.zero;
+
+        other.transform.SetPositionAndRotation(spawnPoint.position, Quaternion.Euler(0, 0, 0));
+    }
+}
