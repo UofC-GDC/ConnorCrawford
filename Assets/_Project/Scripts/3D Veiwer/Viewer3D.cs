c25a80ee79a33b01ac6afb9e62ac8c0c2c06c907
Daniel Doran <ddoran3@gmail.com>
2019-03-14 17:53:14 -0600

    make viewer3d it's own class

---
+++ Assets/_Project/Scripts/3D Veiwer/Viewer3D.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Viewer3D : MonoBehaviour {
+
+	public GameObject active;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Activate(GameObject gameObject) {
+		active = gameObject;
+		active.SetActive(true);
+	}
+	void Deactivate() {
+		if (active != null) {
+			active.SetActive(false);
+			active = null;
+		}
+	}
+	public State Display(GameObject gameObject) {
+		if(gameObject != active) {
+			Deactivate();
+			Activate(gameObject);
+		}
+		if (Input.GetMouseButtonDown(0)) {
+			Deactivate();
+			return null;
+		}
+		return new DoInteractionState();
+	}
+}
