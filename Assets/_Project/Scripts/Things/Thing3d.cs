b3d2f73b291067c674595a6f5e969e1989128bbe
Daniel Doran <ddoran3@gmail.com>
2019-03-14 17:53:14 -0600

    make viewer3d it's own class

--- Assets/_Project/Scripts/Things/Thing3d.cs
+++ Assets/_Project/Scripts/Things/Thing3d.cs
@@ -1,35 +1,12 @@
 ﻿using UnityEngine;
 
 public class Thing3d : Thing {
-	private bool active;
 	public GameObject Model;
-	private GameObject viewer;
-	private Camera camera3d;
-
-	private float rotateScale = 30;
+	private Viewer3D viewer;
 	protected void Start() {
-		active = false;
-		viewer = GameObject.Find("/3D Viewer");
-		camera3d = viewer.GetComponent<Camera>();
+		viewer = FindObjectOfType<Viewer3D>();
 	}
 	public override State Action(StateManager.Env env, ref Player player) {
-
-		if(!active){
-			camera3d.enabled = true;
-			//instance = Instantiate(Model,target);
-			Model.SetActive(true);
-		}
-		if(active){
-			if(Input.GetMouseButtonDown(0)){
-				Model.SetActive(false);
-				active = false;
-				return null;
-			}
-		}
-
-		if(!active){
-			active = true;
-		}
-		return new DoInteractionState();
+		return viewer.Display(Model);
 	}
 }
