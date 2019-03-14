1418430262f5673853942e58f325ee81e8748179
Daniel Doran <ddoran3@gmail.com>
2019-03-14 01:07:56 -0600

    rudementary 3d viewer

---
+++ Assets/_Project/Scripts/Things/Thing3d.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Thing3d : Thing {
+	private bool active;
+	public GameObject Model;
+	private GameObject viewer;
+	private Camera camera3d;
+	private Transform target;
+	private GameObject instance;
+
+	private float rotateScale = 30;
+	protected void Start() {
+		active = false;
+		viewer = GameObject.Find("/3D Viewer");
+		camera3d = GameObject.Find("/3D Viewer/Camera").GetComponent<Camera>();
+		target = GameObject.Find("/3D Viewer/Target").transform;
+	}
+	public override State Action(StateManager.Env env, ref Player player) {
+
+		if(!active){
+			camera3d.enabled = true;
+			instance = Instantiate(Model,target);
+		}
+
+		instance.transform.eulerAngles = calcMouseAngle();
+
+		if(!active){
+			active = true;
+		}
+		return new DoInteractionState();
+	}
+
+	private Vector3 calcMouseAngle() {
+		return new Vector3(
+			rotateScale * (Input.mousePosition.y / Screen.height) - (rotateScale / 2),
+			-(rotateScale * (Input.mousePosition.x / Screen.width) - (rotateScale / 2)),
+			0);
+	}
+}
