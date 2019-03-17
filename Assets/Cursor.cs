cd88bc9bc6519ccef480fcd52d307cc6d616a48b
Phantom-1st <caelum.sloan1@ucalgary.ca>
2019-03-16 23:01:40 -0600

    Okay maybe merge works this time

---
+++ Assets/Cursor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : Singleton<Cursor> 
+{
+    [SerializeField] private AudioSource mouseShake;
+
+    public void ShakeMouse()
+    {
+        mouseShake.Play();
+    }
+}
