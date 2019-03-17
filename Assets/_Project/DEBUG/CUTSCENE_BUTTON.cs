95594f33443b7f00b6f56c7f6580985cec1f0450
Phantom-1st <caelum.sloan1@ucalgary.ca>
2019-03-17 16:29:38 -0600

    Testing cutscenes still

--- Assets/_Project/DEBUG/CUTSCENE_BUTTON.cs
+++ Assets/_Project/DEBUG/CUTSCENE_BUTTON.cs
@@ -21,10 +21,15 @@ public class CUTSCENE_BUTTON : Thing
 
         var line2 = new CutScene.Line();
         line2.arg = door.gameObject;
-        line2.verb = CutScene.Verb.DoAction;
+        line2.verb = CutScene.Verb.DisplayInsight;
+
+        var line3 = new CutScene.Line();
+        line3.arg = door.gameObject;
+        line3.verb = CutScene.Verb.DoAction;
 
         c.script = new List<CutScene.Line>();
         c.script.Add(line1);
+        c.script.Add(line3);
         c.script.Add(line2);
         return c.MakeState();
     }
