3bcb6c27ff8b958e62bb3809d656f373f4389f17
Phantom-1st <caelum.sloan1@ucalgary.ca>
2019-03-17 16:15:02 -0600

    Testing cutscenes

---
+++ Assets/_Project/DEBUG/CUTSCENE_BUTTON.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CUTSCENE_BUTTON : Thing 
+{
+    [SerializeField] private GameObject door;
+
+    public override State Action(StateManager.Env env, ref Player player)
+    {
+        return DoorCutscene();
+    }
+
+    public State DoorCutscene()
+    {
+        CutScene c = (CutScene)ScriptableObject.CreateInstance(typeof(CutScene));
+
+        var line1 = new CutScene.Line();
+        line1.arg = door.gameObject;
+        line1.verb = CutScene.Verb.WalkTo;
+
+        var line2 = new CutScene.Line();
+        line2.arg = door.gameObject;
+        line2.verb = CutScene.Verb.DoAction;
+
+        c.script = new List<CutScene.Line>();
+        c.script.Add(line1);
+        c.script.Add(line2);
+        return c.MakeState();
+    }
+}
