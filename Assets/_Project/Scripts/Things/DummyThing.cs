34845a236dafcbf1108206b336cb149d11b7c688
Phantom-1st <caelum.sloan1@ucalgary.ca>
2019-03-25 22:25:02 -0600

    Added some sounds, and hopefully those nasty thumbs.db and master thing are gone

--- Assets/_Project/Scripts/Things/DummyThing.cs
+++ Assets/_Project/Scripts/Things/DummyThing.cs
@@ -1,1 +1,36 @@
-﻿public class DummyThing : Thing {}
+﻿using UnityEngine;
+
+public class DummyThing : Thing
+{
+    private AudioSource audioSource = null;
+    private bool noAudio = false;
+
+    [Range(0,2)]
+    [SerializeField] private float delayTime=.345f;
+
+    public override State Action(StateManager.Env env, ref Player player)
+    {
+        if (!noAudio)
+        {
+            if (audioSource == null)
+            {
+                var sourceTemp = GetComponent<AudioSource>();
+                if (sourceTemp == null)
+                {
+                    noAudio = true;
+                }
+                else
+                {
+                    audioSource = sourceTemp;
+                    audioSource.PlayDelayed(delayTime);
+                }
+            }
+            else if (!audioSource.isPlaying)
+            {
+                audioSource.PlayDelayed(delayTime);
+            }
+        }
+
+        return base.Action(env, ref player);
+    }
+}
