9c2d95be5e2b955a8c74eadae1965728c543df04
Daniel Doran <ddoran3@gmail.com>
2019-03-27 17:07:27 -0600

    rotate around

--- Assets/_Project/Scripts/3D Veiwer/RotateBase.cs
+++ Assets/_Project/Scripts/3D Veiwer/RotateBase.cs
@@ -2,13 +2,14 @@
 
 public class RotateBase : Interatable3D {
 
+	public Vector3 rotateAxis = Vector3.up;
 	public float rotateSpeed = 400;
 	private float mouseX;
 
 	public override void interact() {
 		float delta = mouseX - Input.mousePosition.x;
 		float modifier = rotateSpeed / Screen.height;//Not a typo, screen HEIGHT
-		transform.Rotate(0, delta * modifier, 0);
+		transform.RotateAround(transform.position, transform.TransformDirection(rotateAxis), delta * modifier);
 		mouseX = Input.mousePosition.x;
 	}
 
