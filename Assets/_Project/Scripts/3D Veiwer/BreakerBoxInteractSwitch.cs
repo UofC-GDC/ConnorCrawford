e3fd59c8f4bbdf41d2060204777b9450d1a302ca
caelum.sloan1 <caelum.sloan1@ucalgary.ca>
2019-04-03 18:52:29 -0600

    Breaker box 3d scripts

---
+++ Assets/_Project/Scripts/3D Veiwer/BreakerBoxInteractSwitch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakerBoxInteractSwitch : Interatable3D
+{
+    [SerializeField] private Vector3 rotationOn;
+    [SerializeField] private Vector3 rotationOff;
+
+    public bool on = false;
+
+    public override void interact(StateManager.Env env, ref Player player)
+    {
+        var rot = on ? rotationOff : rotationOn;
+        transform.rotation = Quaternion.Euler(rot);
+    }
+}
---
+++ Assets/_Project/Scripts/Managers/BreakerBoxManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakerBoxManager : Singleton<BreakerBoxManager>
+{
+    [SerializeField] List<BreakerBoxInteractSwitch> switches = new List<BreakerBoxInteractSwitch>();
+
+    private void Update()
+    {
+        bool everythingOff = false;
+
+        for (int i = 0; i < switches.Count; i++)
+        {
+            if (i == 3 || i == 1 || i == 2 || i == 8) { continue; }
+            if (switches[i].on)
+            {
+                everythingOff = true;
+                break;
+            }
+        }
+
+        if (switches[3].on && switches[1] && switches[2] && switches[8] && everythingOff)
+        {
+            //Turn off all objects that need to be turned off. Turn off lights. Trigger particle effect & sound effects, etc.
+            Destroy(this);
+        }
+    }
+}
