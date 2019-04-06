4cb92e8644c0a9bc99ffca0803e501bdc1b9f1a6
Phantom-1st <caelum.sloan1@ucalgary.ca>
2019-04-06 17:45:47 -0600

    Safe Progress

--- Assets/_Project/Scripts/3D Veiwer/SafeInteract.cs
+++ Assets/_Project/Scripts/3D Veiwer/SafeInteract.cs
@@ -2,39 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SafeInteract : Interatable3D {
-
+public class SafeInteract : Interatable3D
+{
 	public Vector3 rotateAxis = Vector3.right;
 	public float rotateSpeed = 400;
 	private float mouseY;
 	private float angle;
 	private Dictionary<int, char> lockCombo = new Dictionary<int, char>
-        {
-            { 0, 'a'}, { 24, 'b' }, { 48, 'c' },
-			{ 72, 'd'}, { 96, 'e' }, { 120, 'f' },
-			{ 144, 'g'}, { 168, 'h' }, { 192, 'i' },
-			{ 216, 'j'}, { 240, 'k' }, { 264, 'l' },
-			{ 288, 'm'}, { 312, 'n' }, { 336, 'o' }
-        };
+    {
+        { 0, 'a'}, { 24, 'b' }, { 48, 'c' },
+		{ 72, 'd'}, { 96, 'e' }, { 120, 'f' },
+		{ 144, 'g'}, { 168, 'h' }, { 192, 'i' },
+		{ 216, 'j'}, { 240, 'k' }, { 264, 'l' },
+		{ 288, 'm'}, { 312, 'n' }, { 336, 'o' }
+    };
 
-	//Not sure if this is right (what am I doing)
-	public override void interact(StateManager.Env env, ref Player player){
+	public override void interact(StateManager.Env env, ref Player player)
+    {
 		float delta = mouseY - Input.mousePosition.y;
 		float modifier = rotateSpeed / Screen.width; 
 		transform.RotateAround(transform.position, transform.TransformDirection(rotateAxis), delta * modifier);
-		updateFields();
+		UpdateFields();
 	}
 	
-	protected override void interactStart() {
+	protected override void interactStart()
+    {
 		//get mouse's y position
-		updateFields();
+		UpdateFields();
 	}
 
-	protected override void interactEnd() {
+	protected override void interactEnd()
+    {
 		//snap to nearest degree
 	}
 
-	private void updateFields(){
+	private void UpdateFields()
+    {
 		mouseY = Input.mousePosition.y;
 		//angle's origin is the middle of screen 
 		//and measured from positive y-axis instead of x-axis
