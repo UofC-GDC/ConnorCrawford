4949e819e0a93b1009928f72251e41ef19934630
Brian Cruz <renzbrian.cruz@hotmail.com>
2019-04-09 17:23:55 -0600

    Tried my best on the radio interact. Not sure what is missing and RadioSliderInteract.cs still needs work

---
+++ Assets/_Project/Scripts/3D Veiwer/RadioSliderInteract.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/*
+ * Place this script on the radio slider game object
+ */
+public class RadioSliderInteract : Interatable3D
+{
+    [SerializeField] private AudioSource radioStaticAudio;
+    [SerializeField] private AnimationCurve audioCurve;
+
+    private Vector3 startingPos;
+    Vector3 movementVector = Vector3.right;
+    private float movementFactor;
+
+    private void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    public override void interact(StateManager.Env env, ref Player player)
+    {
+        // Still need to work on this
+        movementFactor = Input.mousePosition.x / Screen.height;
+        movementFactor = Mathf.Clamp(movementFactor, 0, 3);
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
