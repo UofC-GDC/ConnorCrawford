d44ef0dc7c76f6d1e0efb0b58286d6278ee2b6cf
Phantom-1st <caelum.sloan1@ucalgary.ca>
2019-04-11 10:56:30 -0600

    clean up clean up

---
+++ Assets/_Project/Scripts/Shaders/ScreenSpaceShader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSpaceShader : MonoBehaviour {
+
+    public Material effect;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, effect);
+    }
+}
