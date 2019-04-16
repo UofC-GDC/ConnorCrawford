e7a32ede56aa7123b38ca121f3a445ca15e2e612
CaelumSloan <caelum.sloan1@ucalgary.ca>
2019-04-16 15:59:54 -0600

    Fixing text glitches in progress

--- Assets/_Project/Scripts/LogoIntro/LogoIntro.cs
+++ Assets/_Project/Scripts/LogoIntro/LogoIntro.cs
@@ -7,6 +7,8 @@ public class LogoIntro : MonoBehaviour
     [SerializeField] private AudioSource logoAudio;
     [SerializeField] private Animator panelAnimator;
     [SerializeField] private Animator logoAnimator;
+    [Range(0,10)]
+    [SerializeField] private float transitionTime = 4f;
 
     private void Awake()
     {
@@ -16,7 +18,7 @@ public class LogoIntro : MonoBehaviour
     void Start ()
     {
         UnityEngine.Cursor.visible = false;
-        AudioManager.Instance.transitionTime = 12;
+        AudioManager.Instance.transitionTime = 3 * transitionTime;
         AudioManager.Instance.MainThemeDay();
         DontDestroyOnLoad(this.gameObject);
         StartCoroutine(Intro());
@@ -25,12 +27,12 @@ public class LogoIntro : MonoBehaviour
     IEnumerator Intro()
     {
         panelAnimator.SetTrigger("FadeFromBlack");
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(transitionTime);
         logoAnimator.SetTrigger("Logo");
         logoAudio.Play();
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(transitionTime);
         panelAnimator.SetTrigger("FadeToBlack");
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(transitionTime*2);
         SceneManager.LoadScene("Morning(Clean)");
     }
 }
