2bebee0c932e58ba0361ba8edb6cdded03aec618
CaelumSloan <caelum.sloan1@ucalgary.ca>
2019-04-17 21:19:54 -0600

    Fixing sound levels

--- Assets/_Project/Scripts/Things/Radio.cs
+++ Assets/_Project/Scripts/Things/Radio.cs
@@ -6,27 +6,31 @@ public class Radio : Thing3d
 {
     [SerializeField] private List<AudioClip> radioSongs;
     [SerializeField] private AudioSource source;
+    [SerializeField] private BatteryInteract battery;
 
     private int i = 0;
 
-    protected new void Start()
-    {
-        base.Start();
-        source.loop = false;
-        source.clip = radioSongs[i];
-        source.Play();
-    }
+    //protected new void Start()
+    //{
+    //    base.Start();
+    //    source.loop = false;
+    //    source.clip = radioSongs[i];
+    //    source.Play();
+    //}
 
     private void Update()
     {
-        if (!source.isPlaying)
-        {
-            if (i >= radioSongs.Count - 1)
-                i = 0;
-            else
-                i++;
-            source.clip = radioSongs[i];
-            source.Play();
-        }
+        //if (!source.isPlaying)
+        //{
+        //    if (i >= radioSongs.Count - 1)
+        //        i = 0;
+        //    else
+        //        i++;
+        //    source.clip = radioSongs[i];
+        //    source.Play();
+        //}
+
+
+        source.enabled = !battery.battery1 || !battery.battery2;
     }
 }
