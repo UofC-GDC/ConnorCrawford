6832ea777f1eda4334ec58a0b25901bff8319195
CaelumSloan <caelum.sloan1@ucalgary.ca>
2019-04-19 16:54:35 -0600

    Added a cute little easter weekend easter egg

---
+++ Assets/_Project/Scripts/NonStateMachineScripts/ShowMe.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMe : MonoBehaviour 
+{
+    private void LateUpdate()
+    {
+        GetComponent<SpriteRenderer>().enabled = !DarknessManager.Instance.roomLightOn && !DarknessManager.Instance.day && !DarknessManager.Instance.doorOpen;
+            }
+}
