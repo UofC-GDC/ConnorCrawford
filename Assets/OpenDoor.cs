f30170b57c30535b768fded71edece35b21ae3fa
CaelumSloan <caelum.sloan1@ucalgary.ca>
2019-04-28 20:38:23 -0600

    Its the sound of da police

--- Assets/OpenDoor.cs
+++ Assets/OpenDoor.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenDoor : Interatable3D 
 {
     [SerializeField] private GameObject origin;
 
     private Vector3 mouseStartPos;
+    [SerializeField]  private Slider slider;
 
     protected override void interactStart()
     {
@@ -15,6 +17,8 @@ public class OpenDoor : Interatable3D
 
     public override void interact(StateManager.Env env, ref Player player)
     {
+        if (slider.value <= slider.maxValue / 2f)
+            return;
         origin.transform.Rotate(origin.transform.forward, (Input.mousePosition - mouseStartPos).x);
     }
 }
