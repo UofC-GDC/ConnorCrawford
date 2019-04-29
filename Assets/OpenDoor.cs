72a64f4b837e71574cb78f52274c5af6eb2359e4
CaelumSloan <caelum.sloan1@ucalgary.ca>
2019-04-28 20:28:59 -0600

    Everything Again

--- Assets/OpenDoor.cs
+++ Assets/OpenDoor.cs
@@ -6,8 +6,15 @@ public class OpenDoor : Interatable3D
 {
     [SerializeField] private GameObject origin;
 
+    private Vector3 mouseStartPos;
+
+    protected override void interactStart()
+    {
+        mouseStartPos = Input.mousePosition;
+    }
+
     public override void interact(StateManager.Env env, ref Player player)
     {
-        
+        origin.transform.Rotate(origin.transform.forward, (Input.mousePosition - mouseStartPos).x);
     }
 }
