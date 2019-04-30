77597be40c9564c5856eab6f2868e45e0d53a8af
CaelumSloan <caelum.sloan1@ucalgary.ca>
2019-04-29 18:04:53 -0600

    Pushin like I'm having a baby

---
+++ Assets/_Project/Scripts/ClickMask.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickMask : MonoBehaviour 
+{
+
+    [SerializeField] private Sprite leftClick;
+    [SerializeField] private Sprite rightClick;
+    [SerializeField] private Sprite full;
+
+    private SpriteMask spriteMask;
+
+    private void Start () 
+    {
+        spriteMask = GetComponent<SpriteMask>();
+    }
+	
+	private void Update () 
+    {
+        Sprite sprite = null;
+
+        if (Input.GetMouseButton(0)) sprite = leftClick;
+        if (Input.GetMouseButton(1)) sprite = rightClick;
+        if (Input.GetMouseButton(0) && Input.GetMouseButton(1)) sprite = full;
+
+        spriteMask.sprite = sprite;
+    }
+}
